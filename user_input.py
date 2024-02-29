0785419258f895e90c18b0b4feb5cead7c6e5d45
alvviinn <alkimani25@gmail.com>
2024-02-29 18:09:57 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = str(input('What is your name: '))
+age = int(input('What is your age:'))
+location = str(input('What is your location:'))
+
+print('Hello',name,'you are',age,'years old and live in',location)
